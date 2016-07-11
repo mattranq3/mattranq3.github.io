65d9e6c51be1a7bcf4ffbc460b1c7552e82fe02c
mattranq3 <phamnhuvu967@gmail.com>
2016-07-11 17:44:29 +0700

    Update agency.js

--- js/agency.js
+++ js/agency.js
@@ -4,13 +4,20 @@
     "use strict"; // Start of use strict
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
-    });
+    $(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
 
     // Highlight the top nav as scrolling occurs
     $('body').scrollspy({
